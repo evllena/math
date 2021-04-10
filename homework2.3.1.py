eb8e97acebef4ba4f818ef602ffac02af143f2bf
evllena <helen.ermolaeva7@gmail.com>
2021-04-10 14:04:26 +1100

    1

--- homework2.3.1.py
+++ homework2.3.1.py
@@ -2,9 +2,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 from math import pi
 
-fig, ax = plt.subplots(1, 3, figsize=(12, 6))
-circle = plt.Circle((0, 0), 5,color='r', fill=False)
+
+circle = plt.Circle((0, 0), 5, color='r', fill=False)
 ax = plt.gca()
 ax.add_patch(circle)
 plt.axis('scaled')
+plt.grid(color='lightgray',linestyle='--')
 plt.show()
